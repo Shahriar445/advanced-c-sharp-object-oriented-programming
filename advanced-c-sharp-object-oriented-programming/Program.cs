b2080c345615bf26a3fb8b9dd37ec659b77e3b0f
Shahriar Haque <shahriarhaque445@gmail.com>
2024-05-15 00:47:01 +0600

    Defining classes

--- advanced-c-sharp-object-oriented-programming/Program.cs
+++ advanced-c-sharp-object-oriented-programming/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Rectangel obj = new Rectangel(10, 20);
+Rectangel obj2 = new Rectangel(30);
+
+Console.WriteLine("Area of Rectangel height and width: " + obj.getArea());
+Console.WriteLine("Area of Rectangel with side: " + obj2.getArea());
+
+obj.height = 2;
+obj.width = 3;
+Console.WriteLine("After chang height and width: " + obj.getArea());
---
+++ advanced-c-sharp-object-oriented-programming/Rectangel.cs
@@ -0,0 +1,24 @@
+﻿
+class Rectangel
+{
+    public int width;
+    public int height;
+
+    public Rectangel(int w, int h)
+    {
+        width = w;
+        height = h;
+    }
+
+    public Rectangel(int side)
+    {
+        width = height
+            = side;
+    }
+
+    public int getArea()
+    {
+        return width * height;
+    }
+}
+
