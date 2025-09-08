6fa637aa64d9911e64d7d29f809628bd24587e43
Shahriar Haque <shahriarhaque445@gmail.com>
2025-09-08 14:29:40 +0600

    update.
    Add Chap_1 namespace with EasyCode and Person classes
    
    Introduces a new namespace `Chap_1` containing two public classes:
    `EasyCode` and `Person`. The `Person` class features a `Name` property,
    a computed `Greeting` property for personalized greetings, and an
    override of the

---
+++ Chap_1/EasyCode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chap_1;
+
+public class EasyCode
+{
+    
+}
+
+public class Person
+{
+    public string Name { get; set; }
+    public string Greeting => $"Hello,{Name}!";
+    public override string ToString() => Name;
+}
